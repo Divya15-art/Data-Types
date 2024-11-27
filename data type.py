f4c033a0d135bf0da321d890badd8f3ec86a31bb
@Divya15-art <chandakdivya14@gmail.com>
2024-11-27 22:00:08 +0530

    first commit

---
+++ data type.py
@@ -0,0 +1,16 @@
+a=5
+a1=float(a)
+print("The answer for question a is",type(a))
+print("The answer for question a1 is",type(a1))
+b=4.8
+b1=bool(b)
+print("The answer for question b is",type(b))
+print("The answer for question b1 is",type(b1))
+c=True
+c1=str(c)
+print("The answer for question c is",type(c))
+print("The answer for question c1 is",type(c1))
+d="My name is Divya"
+d1=bool(d)
+print("The answer for question d is",type(d))
+print("The answer for question d1 is",type(d1))
